9d01b50992c71f1d59959ee7f20b3b9b4268e486
Noëlle Gagnaire <nonomimik4@gmail.com>
2020-01-31 14:25:40 +0100

    Remove all

--- README.md
+++
@@ -1,6 +0,0 @@
-# Shiny Data Exploration
-
-A Shiny application that shows univariate and bivariate statistics of a dataset.
-
-<!---![Univariate statistics](univariate.png)
-![Bivariate statistics](bivariate.png)--->
--- ui/sidebar.R
+++
@@ -1,8 +0,0 @@
-sidebar <- dashboardSidebar(sidebarMenu(
-  menuItem(
-    "Univariate statistics",
-    tabName = "uni",
-    icon = icon("dashboard")
-  ),
-  menuItem("Bivariate statistics", tabName = "bi", icon = icon("th"))
-))
